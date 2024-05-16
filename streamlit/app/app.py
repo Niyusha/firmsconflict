4de3070a543112b5426e88322477cbe2b116cc46
blopez115 <90004254+blopez115@users.noreply.github.com>
2024-05-16 09:40:41 -0700

    Update app.py
    We are going to add all of the fires and losses onto the same map regardless of date.

--- streamlit/app/app.py
+++ streamlit/app/app.py
@@ -2,38 +2,71 @@
 
 import pandas as pd
 import streamlit as st
+import folium
+import glob
+import os
+from datetime import datetime
 
-# TODO: add a way to organize the coordinates by date.
-
-def get_coords_by_date(data_path) -> dict:
+from streamlit_folium import st_folium
+    
+def get_all_coords() -> list:
     # We will first organize the coordinates by date.
-    df = pd.read_csv(data_path)
+    df = pd.read_csv('cleaned_data.csv')
 
     # The number of rows in the CSV file.
     row_num = df.shape[0]
 
-    # We will convert all of the dates to datetime format instead of strings.
-    df['acq_date'] = pd.to_datetime(df['acq_date'])
-
-    coordinates = dict()
+    coordinates = list()
     for i in range(row_num):
-        # The curr_date variable will represent the date that our loop is on.
-        curr_date = df['acq_date'][i]
-        # If we do not have the acq_date key in the curr_date dictionary for organizing the
-        # coordinates based on date, we will declare a new list of dictionaries.
-        if not curr_date in coordinates.keys():
-            coordinates[curr_date] = list[dict]()
-
         # We will append a coordinate to the array.
-        coordinates[curr_date].append({
+        coordinates.append({
             'latitude': df['latitude'][i],
-            'longitude': df['longitude'][i]
+            'longitude': df['longitude'][i],
+            'type': 'fire'
         })
+    
+    # List all KML files in the directory
+    json_files = [ 'doc.json' ]
+    for json_file in json_files:
+        # TODO: Add pandas code for doing statistical analysis of the JSONs.
+        df = pd.read_json(json_file)
+
+        for index, row in df.iterrows():
+            coordinate=row['coordinates'].split(',')[:2]
+            # We will append a coordinate to the array.
+            coordinates.append({
+                'latitude': float(coordinate[0]),
+                'longitude': float(coordinate[1]),
+                'type': 'loss'
+            })
 
     return coordinates
 
-coordinates = get_coords_by_date('cleaned_data.csv')
-selected_entry = st.sidebar.selectbox('Select date', coordinates.keys())
-selected_date_coords = coordinates[selected_entry]
 
-st.map(selected_date_coords)
+coordinates = get_all_coords()
+
+st.title(f'Map of losses and fires in Ukraine based on CSVs and KMLs')
+m = folium.Map(location=[coordinates[0]['latitude'], coordinates[0]['longitude']], zoom_start=10)
+for coord in coordinates:
+    if coord['type'] == 'fire':
+        folium.CircleMarker(
+            location=[coord['latitude'],coord['longitude']],
+            radius=10,
+            color='orange',
+            fill=False,
+            fill_color='orange',
+            fill_opacity=0.6
+        ).add_to(m)
+    else:
+        folium.CircleMarker(
+            location=[coord['latitude'],coord['longitude']],
+            radius=25,
+            color='blue',
+            fill=True,
+            fill_color='blue',
+            fill_opacity=0.6
+        ).add_to(m)
+
+
+st_folium(m)
+st.write("The fires are marked in red while the losses are marked in blue.")
